0b6995af241550c81abc9faf970e924a3224b438
Chuan Li <lichuan0620@gmail.com>
2017-03-05 10:53:36 -0800

    models updated and migrated

--- CatoApp/models.py
+++ CatoApp/models.py
@@ -1,36 +1,57 @@
 from django.db import models
+from django.forms import ModelForm
+
+
+# education options
+HIGH_SCHOOL = 'HI'
+UNDERGRADUATE = 'UG'
+BACHELOR = 'BC'
+GRADUATE = 'GD'
+MASTER = 'MA'
+PHD = 'PD'
+DOCTOR = 'DC'
+EDUCATION_CHOICES = (
+    (HIGH_SCHOOL, 'High school'),
+    (UNDERGRADUATE, 'Undergraduate student'),
+    (BACHELOR, 'Bachelor'),
+    (GRADUATE, 'Graduate student'),
+    (MASTER, 'Master'),
+    (PHD, 'PHD student'),
+    (DOCTOR, 'Doctor'),
+)
+# end education
+
+# major options
+COMPUTER_SCIENCE = 'CS'
+SOFTWARE_ENGINEERING = 'SE'
+COMPUTER_ENGINEERING = 'CE'
+COMPUTER_SCIENCE_AND_ENGINEERING = 'CA'
+OTHER_STEM = 'OS'
+NON_STEM = 'NS'
+MAJOR_CHOICES = (
+    (COMPUTER_SCIENCE, 'Computer Science'),
+    (SOFTWARE_ENGINEERING, 'Software Engineering'),
+    (COMPUTER_ENGINEERING, 'Computer Engineering'),
+    (COMPUTER_SCIENCE_AND_ENGINEERING, 'Computer Science and Engineering'),
+    (OTHER_STEM, 'Other STEM majors'),
+    (NON_STEM, 'Other non-STEM majors'),
+)
+# end major
 
 
 class User(models.Model):
-    email = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=16)
     # education = models.CharField(max_length=16,null=True)
-    # education with limited choices
-    HIGH_SCHOOL = 'HI'
-    UNDERGRADUATE = 'UG'
-    BACHELOR = 'BC'
-    GRADUATE = 'GD'
-    MASTER = 'MA'
-    PHD = 'PD'
-    DOCTOR = 'DC'
-    EDUCATION_CHOICES = (
-        (HIGH_SCHOOL, 'High school'),
-        (UNDERGRADUATE, 'Undergraduate student'),
-        (BACHELOR, 'Bachelor'),
-        (GRADUATE, 'Graduate student'),
-        (MASTER, 'Master'),
-        (PHD, 'PHD student'),
-        (DOCTOR, 'Doctor')
-    )
     education = models.CharField(
         max_length=2,
         choices=EDUCATION_CHOICES,
-        default=UNDERGRADUATE
+        default=UNDERGRADUATE,
+        null=True
     )
-    # end education
     graduation_date = models.DateField(null=True)
     major = models.CharField(max_length=16,null=True)
-    zipcode = models.PositiveIntegerField()
+    zipcode = models.PositiveIntegerField(null=True)
 
 
 class Skill(models.Model):
